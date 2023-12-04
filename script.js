234ee94df0bcff4de61343be0eb86061a1c07422
Tyler Ralston <tralston511@gmail.com>
2023-12-03 19:54:38 -0500

    functioning password generator complete

--- script.js
+++ script.js
@@ -2,32 +2,60 @@ const characterAmountRange = document.getElementById('characterAmountRange')
 
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumberElement = document.getElementById('includeNumbers')
+const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSpecialCharactersElement = document.getElementById('includeSpecialCharacters')
 const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+// Character codes for functions for password generation
+const UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57)
+const SPECIAL_CHARACTER_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(arrayFromLowtoHigh(58, 64))
 
 // Script to make slider and number counter function together:
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-function syncCharacterAmount(e) {
-  const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
-}
-
 // Script to make generate password button generate a password when clicked:
-form.addEventListener('submit'e => {
+
+  form.addEventListener('submit', e => {
     e.preventDefault()
     const characterAmount = characterAmountNumber.value
     const includeUppercase = includeUppercaseElement.checked
     const includeNumbers = includeNumbersElement.checked
     const includeSpecialCharacters = includeSpecialCharactersElement.checked
     const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSpecialCharacters)
+    passwordDisplay.innerText = password
   })
-// Script to generate password:
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSpecialCharacters){
   
-}
+  // Script to generate password:
 
+  function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSpecialCharacters){
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includeSpecialCharacters) charCodes = charCodes.concat(SPECIAL_CHARACTER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++){
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+  }
+
+  function arrayFromLowtoHigh(low, high){
+  const array = []
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array 
+  }
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
